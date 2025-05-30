a978927999da770a645deda05e772ff0fdde9a5a
adrianfa-f <adrianfrancoavila1995@gmail.com>
2025-05-30 17:18:25 -0400

    Corregida url de cors sin barra final

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express();
 // Middlewares
 app.use(cors(
     {
-    origin:'https://bookink.netlify.app/',
+    origin:'https://bookink.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 app.use(helmet());
